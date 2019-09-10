7e123f9d362403d22e6d4b0dbcb013863f14c616
sahilmann98 <54046468+sahilmann98@users.noreply.github.com>
2019-09-11 05:29:45 +1200

    Add files via upload

---
+++ RussianRouletteTests/GameTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RussianRoulette;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RussianRoulette.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void btnShoot_ClickTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void btnSpin_ClickTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
